15d55cf6476d286b8409fc31552340effe5d4160
Anderson Ito <andersonkmi@acm.org>
2018-02-04 16:45:52 -0200

    Initial commit

---
+++ src/main/java/org/sharpsw/leetcode/LongestSubstringExercise.java
@@ -0,0 +1,8 @@
+package org.sharpsw.leetcode;
+
+public class LongestSubstringExercise {
+    public int lengthOfLongestSubstring(String s) {
+        int maxLength = 0;
+        return maxLength;
+    }
+}
