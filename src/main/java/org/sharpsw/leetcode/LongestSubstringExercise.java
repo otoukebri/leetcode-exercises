d6477562c2707d7dfa2db2ad28f9bc355d97b357
Anderson Ito <andersonkmi@acm.org>
2018-02-04 17:32:22 -0200

    Initial version - not yet finished

--- src/main/java/org/sharpsw/leetcode/LongestSubstringExercise.java
+++ src/main/java/org/sharpsw/leetcode/LongestSubstringExercise.java
@@ -1,8 +1,33 @@
 package org.sharpsw.leetcode;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LongestSubstringExercise {
     public int lengthOfLongestSubstring(String s) {
         int maxLength = 0;
+        List<Character> myString = convertString(s);
+        StringBuilder buffer = new StringBuilder();
+
+        int index = 0;
+        while(index < myString.size()) {
+            Character character = myString.get(index);
+
+            if(buffer.toString().indexOf(character.charValue()) != -1) {
+                buffer = new StringBuilder().append(character.toString());
+            } else {
+                buffer.append(character.toString());
+                if(buffer.length() > maxLength) {
+                    maxLength = buffer.length();
+                }
+            }
+            index++;
+        }
+
         return maxLength;
+    }
+
+    private List<Character> convertString(String input) {
+        return input.chars().mapToObj(c -> (char) c).collect(Collectors.toList());
     }
 }
---
+++ src/test/java/org/sharpsw/leetcode/LongestSubstringExerciseTest.java
@@ -0,0 +1,35 @@
+package org.sharpsw.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class LongestSubstringExerciseTest {
+    LongestSubstringExercise classUnderTest;
+
+    @Before
+    public void setup() {
+        classUnderTest = new LongestSubstringExercise();
+    }
+
+    @Test
+    public void testCase001() {
+        assertThat(classUnderTest.lengthOfLongestSubstring("abca"), is(3));
+    }
+
+    @Test
+    public void testCase002() {
+        assertThat(classUnderTest.lengthOfLongestSubstring("bbbbbbb"), is(1));
+    }
+
+    @Test
+    public void testCase003() {
+        assertThat(classUnderTest.lengthOfLongestSubstring("aabcd"), is(4));
+    }
+
+    @Test
+    public void testCase004() {
+        assertThat(classUnderTest.lengthOfLongestSubstring("pwwkew"), is(3));
+    }
+}
