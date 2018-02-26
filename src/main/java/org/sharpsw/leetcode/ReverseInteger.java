dd83dd636c3a77f8cb6c4eaccdf44e4b24df4961
Anderson Ito <andersonkmi@acm.org>
2018-02-26 08:18:02 -0300

    adding unit tests

--- src/main/java/org/sharpsw/leetcode/ReverseInteger.java
+++ src/main/java/org/sharpsw/leetcode/ReverseInteger.java
@@ -3,16 +3,18 @@ package org.sharpsw.leetcode;
 public class ReverseInteger {
     public int reverse(int number) {
         String str = String.valueOf(number);
-        String reversed = new StringBuilder(str).reverse().toString();
+        boolean flag = str.charAt(0) == '-';
 
+        if(str.length() == 1 || (str.length() == 2 && str.charAt(0) == '-')) {
+            return number;
+        }
+
+        String reversed = new StringBuilder(str).reverse().toString();
         StringBuilder cleaned = new StringBuilder();
         int length = reversed.length();
-        boolean flag = false;
         for(int index = 0; index < length; index++) {
             if(Character.isDigit(reversed.charAt(index))) {
                 cleaned.append(reversed.charAt(index));
-            } else if(reversed.charAt(index) == '-') {
-                flag = true;
             }
         }
 
