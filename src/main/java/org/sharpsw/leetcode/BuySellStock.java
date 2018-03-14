a11e31f988398819f19c5c4385085f24ff5b3763
Anderson Ito <andersonkmi@acm.org>
2018-03-14 07:40:34 -0300

    added one more exercise

--- src/main/java/org/sharpsw/leetcode/BuySellStock.java
+++ src/main/java/org/sharpsw/leetcode/BuySellStock.java
@@ -2,27 +2,14 @@ package org.sharpsw.leetcode;
 
 public class BuySellStock {
     public int maxProfit(int[] prices) {
-        if(prices.length == 0) {
-            return 0;
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
         }
-
-        int lowestValue = prices[0];
-        int lowestValueIndex = 0;
-
-        for(int index = 1; index < prices.length; index++) {
-            if(prices[index] <= lowestValue) {
-                lowestValue = prices[index];
-                lowestValueIndex = index;
-            }
-        }
-
-        int highestProfit = 0;
-        for(int index = lowestValueIndex + 1; index < prices.length; index++) {
-            int profit = prices[index] - lowestValue;
-            if(profit > highestProfit) {
-                highestProfit = profit;
-            }
-        }
-        return highestProfit;
+        return maxprofit;
     }
 }
