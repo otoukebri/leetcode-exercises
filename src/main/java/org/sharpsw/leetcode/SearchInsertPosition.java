b42b8d084fb864e629d74a57b3bf7e1d9fc097ce
Anderson Ito <andersonkmi@acm.org>
2018-03-15 22:38:00 -0300

    Added exercise insert position

--- src/main/java/org/sharpsw/leetcode/SearchInsertPosition.java
+++ src/main/java/org/sharpsw/leetcode/SearchInsertPosition.java
@@ -2,6 +2,14 @@ package org.sharpsw.leetcode;
 
 public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
-        return 0;
+        if(nums.length == 0) {
+            return 0;
+        }
+
+        int index = 0;
+        while(index < nums.length && nums[index] < target) {
+            index++;
+        }
+        return index;
     }
 }
--- src/test/java/org/sharpsw/leetcode/SearchInsertPositionTest.java
+++ src/test/java/org/sharpsw/leetcode/SearchInsertPositionTest.java
@@ -19,4 +19,28 @@ public class SearchInsertPositionTest {
 
         assertThat(service.searchInsert(numbers, 0), is(0));
     }
+
+    @Test
+    public void testCase001() {
+        int[] numbers = {1, 3, 5, 6};
+        assertThat(service.searchInsert(numbers, 5), is(2));
+    }
+
+    @Test
+    public void testCase002() {
+        int[] numbers = {1, 3, 5, 6};
+        assertThat(service.searchInsert(numbers, 2), is(1));
+    }
+
+    @Test
+    public void testCase003() {
+        int[] numbers = {1, 3, 5, 6};
+        assertThat(service.searchInsert(numbers, 1), is(0));
+    }
+
+    @Test
+    public void testCase004() {
+        int[] numbers = {1, 3, 5, 6};
+        assertThat(service.searchInsert(numbers, 7), is(4));
+    }
 }
