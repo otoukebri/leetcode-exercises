2bc65f36070ba3ad53896390018997900aeecc63
Anderson Ito <andersonkmi@acm.org>
2018-03-17 10:23:35 -0300

    more code

--- src/test/java/org/sharpsw/leetcode/RemoveElementTest.java
+++ src/test/java/org/sharpsw/leetcode/RemoveElementTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-
 public class RemoveElementTest {
     private RemoveElement service;
 
